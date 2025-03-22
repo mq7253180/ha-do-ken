f92e87dbba89c38f39643bebd0e8efa8443e183e
mq7253180 <mq7253180@126.com>
2025-03-22 17:55:07 +0800

    xxx

--- simulator-springboot/src/main/java/com/xxx/SpringBootConfiguration.java
+++ simulator-springboot/src/main/java/com/xxx/SpringBootConfiguration.java
@@ -3,7 +3,9 @@ package com.xxx;
 import javax.annotation.PostConstruct;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
+@PropertySource(value = {"classpath:application-sensitiveness.properties"})
 @Configuration
 public class SpringBootConfiguration {
     @PostConstruct
