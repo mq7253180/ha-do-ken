9d113096eaea20487545222d488a42cc96d58d11
mq7253180 <mq7253180@126.com>
2025-03-23 17:50:58 +0800

    xxx

--- simulator-springboot/src/main/java/com/xxx/SpringBootConfiguration.java
+++ simulator-springboot/src/main/java/com/xxx/SpringBootConfiguration.java
@@ -2,14 +2,19 @@ package com.xxx;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.session.web.http.DefaultCookieSerializer;
 
 @PropertySource(value = {"classpath:application-sensitiveness.properties"})
 @Configuration
 public class SpringBootConfiguration {
+	@Autowired
+    private DefaultCookieSerializer cookieSerializer;
+
     @PostConstruct
     public void init() {
-    	
+    	cookieSerializer.setUseHttpOnlyCookie(false);
     }
 }
