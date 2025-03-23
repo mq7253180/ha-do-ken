5ba79c8397850e83f5bb37a6886b6984016b94b5
mq7253180 <mq7253180@126.com>
2025-03-23 17:42:25 +0800

    xxx

--- cart-order-service/src/main/java/com/xxx/controller/SampleController.java
+++ cart-order-service/src/main/java/com/xxx/controller/SampleController.java
@@ -1,11 +1,9 @@
 package com.xxx.controller;
 
-
 import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
