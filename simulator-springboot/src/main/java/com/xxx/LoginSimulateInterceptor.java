a1404e3930409fe007351d6fd24eb2b7b2a3d181
mq7253180 <mq7253180@126.com>
2025-03-23 14:46:58 +0800

    xxx

--- simulator-springboot/src/main/java/com/xxx/LoginSimulateInterceptor.java
+++ simulator-springboot/src/main/java/com/xxx/LoginSimulateInterceptor.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
 import com.quincy.sdk.AuthHelper;
-import com.quincy.sdk.annotation.CustomizedInterceptor;
+import com.quincy.sdk.annotation.CustomizedBeforeAuthInterceptor;
 import com.quincy.sdk.o.User;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -13,14 +13,14 @@ import jakarta.servlet.http.HttpServletResponse;
 /**
  * 模拟登录
  */
-@CustomizedInterceptor(pathPatterns = "/**", order = 0)
+@CustomizedBeforeAuthInterceptor(pathPatterns = "/**", order = 0)
 @Component
 public class LoginSimulateInterceptor extends HandlerInterceptorAdapter {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
 		System.out.println("LoginSimulateInterceptor============"+request.getRequestURI());
-//		request.getSession();
-//		AuthHelper.setUser(new User());
+		request.getSession();
+		AuthHelper.setUser(request, new User());
 		return true;
 	}
 }
